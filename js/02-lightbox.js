52b202d6659d9da160ccb41bc990820598459c44
Roman Havryliuk <gavru4r@gmail.com>
2021-12-23 14:15:09 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs  = {
+galeryList: document.querySelector(".gallery")
+}
+
+const galeryMarcup = galleryItems.map(({ description, original, preview }) => {
+    `<li><a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a></li>`
+})
